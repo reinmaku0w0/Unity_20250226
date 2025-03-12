6c7faed94f1ef4c27107bcde7d93daec8cbf19f5
reinmaku0w0 <thbewced01@gmail.com>
2025-03-12 11:53:48 +0800

    偵測與碰撞續

--- Assets/Script/Script2.cs
+++ Assets/Script/Script2.cs
@@ -5,13 +5,26 @@ using UnityEngine;
 
 public class Script2 : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D other)
+    [SerializeField] private GameObject ccc;
+    private void OnCollisionEnter2D(Collision2D col)
     {
         Debug.Log(message:"完美落地!10分!!");
+        //碰到後隱藏自己
+        //gameObject.SetActive(false);
+        //碰到後隱藏對方
+        //col.gameObject.SetActive(false);
     }
     
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerEnter2D(Collider2D col)
     {
         Debug.Log(message:"偵測到了!!");
+        
+        //偵測到的變數名稱
+        Debug.Log(col.gameObject.name);
+        
+        //物件不隱藏才能偵測
+        //GameObject CC = GameObject.Find("圓");
+        
+        ccc.SetActive(true);
     }
 }
