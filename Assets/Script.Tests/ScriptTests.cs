d04a3907591d8bbf9e7b5527246af5ea71a8a8d2
reinmaku0w0 <thbewced01@gmail.com>
2025-03-19 12:13:18 +0800

    測試程式碼

---
+++ Assets/Script.Tests/ScriptTests.cs
@@ -0,0 +1,18 @@
+
+using NUnit.Framework;
+
+public class ScriptTests
+{
+    [Test(Description = "測試兩個數字相加")]
+    [TestCase(2, 2, 4)]
+    [TestCase(4, 4, 4)]
+    public void Add_Two_Number(int a, int b, int expected)
+    {
+        //建立變數
+        TimeConverter timeConverter = new TimeConverter();
+        //呼叫方法
+        int result = timeConverter.Add(a,b);
+        //驗證結果
+        Assert.AreEqual(expected, actual: result);
+    }
+}
